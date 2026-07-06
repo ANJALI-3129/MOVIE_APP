4f23d0f9440cb8d922344bf3bbaf3d486bfababf
Anjali <anjalikumari37417@gmail.com>
2026-07-06 16:21:45 +0530

    movie app

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA4PCnt0qul50qkSomVuB9D0ImCpJw_q1k",
-  authDomain: "movie-app-d7980.firebaseapp.com",
-  projectId: "movie-app-d7980",
-  storageBucket: "movie-app-d7980.firebasestorage.app",
-  messagingSenderId: "189321716843",
-  appId: "1:189321716843:web:5ffa0a44d0117ca918fd2e",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
